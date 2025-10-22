e50c905c07a5d2e5675e4fd8fa16c67bc6d5a3c0
pcannon09 <pcannon775@gmail.com>
2025-10-22 10:16:48 +0200

    Init Commit

---
+++ inc/cvec/cvec.h
@@ -0,0 +1,132 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define CVEC_NPOS 						-1
+#define CVEC_SUCCESS 					0
+#define CVEC_FAIL 						1
+#define CVEC_FAIL_ALLOCATION 			2  		// Or reallocation
+#define CVEC_FORCECAP_FAIL 				3
+#define CVEC_ELEM_NOT_FOUND 			4
+#define CVEC_GET_NULL  					5
+#define CVEC_OUT_BOUNDS  				6
+#define CVEC_NOT_ENOUGH_CAP 			7
+
+#define __CVEC_CAP_ADDITION 			1
+
+// NOTE: Other macro names are here, such as:
+// CVEC_EXPOSE_PRIVATE_API
+
+#define __CVEC_SET_NULL(vec, ptrSym) \
+	vec ptrSym size = 0;             \
+	vec ptrSym cap = 0;              \
+	vec ptrSym elemLen = 0;          \
+	vec ptrSym forceCap = false;     \
+	vec ptrSym dynamicCap = false;    \
+	vec ptrSym initialized = false   \
+
+typedef struct
+{
+	bool initialized;
+
+	void *data;
+
+	bool forceCap;
+	bool dynamicCap;
+
+	size_t size;
+	size_t cap;
+	size_t elemLen;
+} CVEC;
+
+/* MAIN FUNCTIONS */
+// PUBLIC
+CVEC cvec_init(int _cap, size_t _elemSize);
+CVEC cvec_initCopy(const CVEC *_src);
+
+int cvec_destroy(CVEC *_vec);
+int cvec_merge(CVEC *_toMerge, const CVEC *_input);
+int cvec_remove(CVEC *_vec, const size_t _index);
+int cvec_popBack(CVEC *_vec);
+int cvec_popFront(CVEC *_vec);
+int cvec_reverse(CVEC *_vec);
+int cvec_shrink(CVEC *_vec);
+
+bool cvec_at(const CVEC *_vec, const size_t _index);
+bool cvec_atCap(const CVEC *_vec, const size_t _index);
+
+void *cvec_get(const CVEC *_vec, const size_t _index);
+
+// PRIVATE
+int __cvec_push(CVEC *_vec, void *_elem);
+int __cvec_pushFront(CVEC *_vec, void *_elem);
+int __cvec_pushIndex(CVEC *_vec, const size_t _index, void *_elem);
+int __cvec_set(CVEC *_vec, const size_t _index, void *_set);
+int __cvec_delIndex(CVEC *_vec, void *_elem, const size_t start);
+int __cvec_del(CVEC *_vec, void *_elem);
+int __cvec_find(const CVEC *_vec, void *_find);
+
+bool __cvec_hasEnoughCap(const CVEC *_vec, const size_t _additions);
+
+/**/
+
+#define __CVEC_MACRO_MOD_CALL(func, vec, type, val) \
+({ \
+	type _CVEC_TMP_X_VAR_ = val; \
+	int _CVEC_TMP_RETVAL_ = CVEC_NOT_ENOUGH_CAP; \
+	if (__cvec_hasEnoughCap(vec, 1)) \
+		_CVEC_TMP_RETVAL_ = func(vec, &_CVEC_TMP_X_VAR_); \
+	_CVEC_TMP_RETVAL_; \
+})
+
+// Capacity modifiers
+#define cvec_push(vec, type, val) \
+	__CVEC_MACRO_MOD_CALL(__cvec_push, vec, type, val)
+
+#define cvec_pushFront(vec, type, val) \
+	__CVEC_MACRO_MOD_CALL(__cvec_pushFront, vec, type, val)
+
+#define cvec_pushIndex(vec, type, pos, val) \
+({ \
+	type _CVEC_TMP_X_VAR_ = val; \
+	int _CVEC_TMP_RETVAL_; \
+	if (__cvec_hasEnoughCap(vec, 1)) \
+		_CVEC_TMP_RETVAL_  = __cvec_pushIndex(vec, pos, &_CVEC_TMP_X_VAR_); \
+	else \
+		_CVEC_TMP_RETVAL_ = CVEC_NOT_ENOUGH_CAP; \
+	_CVEC_TMP_RETVAL_; \
+})
+
+////
+
+#define cvec_set(vec, type, val, index) \
+({ \
+	type _CVEC_TMP_X_VAR_ = val; \
+	int _CVEC_TMP_RETVAL_ = __cvec_set(vec, index, &_CVEC_TMP_X_VAR_); \
+	_CVEC_TMP_RETVAL_; \
+})
+
+#define cvec_delIndex(vec, type, val, index) \
+({ \
+	type _CVEC_TMP_X_VAR_ = val; \
+	int _CVEC_TMP_RETVAL_ = __cvec_delIndex(vec, &_CVEC_TMP_X_VAR_, index); \
+	_CVEC_TMP_RETVAL_; \
+})
+
+#define cvec_del(vec, type, val) \
+({ \
+	type _CVEC_TMP_X_VAR_ = val; \
+	int _CVEC_TMP_RETVAL_ = __cvec_del(vec, &_CVEC_TMP_X_VAR_); \
+	_CVEC_TMP_RETVAL_; \
+})
+
+#define cvec_find(vec, type, val) \
+({ \
+	type _CVEC_TMP_X_VAR_ = val; \
+	int _CVEC_TMP_RETVAL_ = __cvec_find(vec, &_CVEC_TMP_X_VAR_); \
+	_CVEC_TMP_RETVAL_; \
+})
+
---
+++ src/cvec.c
@@ -0,0 +1,425 @@
+#include "../inc/cvec/cvec.h"
+
+CVEC cvec_init(int _cap, size_t _elemSize)
+{
+	CVEC v;
+
+	v.forceCap = false;
+ 	v.dynamicCap = true;
+
+	v.size = 0;
+	v.elemLen = _elemSize;
+	v.cap = (_cap < 0 ? v.size + __CVEC_CAP_ADDITION : _cap);
+	v.data = malloc(v.elemLen * v.cap);
+
+	// Failed to malloc
+	if (!v.data)
+	{
+		v.initialized = false;
+
+		return v;
+	}
+
+	v.initialized = true;
+
+	return v;
+}
+
+int cvec_destroy(CVEC *_vec)
+{
+	if (!_vec || !_vec->data) return CVEC_FAIL;
+
+	free(_vec->data); _vec->data = NULL;
+
+	__CVEC_SET_NULL(_vec, ->);
+
+	return CVEC_SUCCESS;
+}
+
+int __cvec_push(CVEC *_vec, void *_elem)
+{
+	if (!_vec || !_elem) return CVEC_FAIL;
+
+	if (_vec->size >= _vec->cap)
+	{
+		if (_vec->forceCap)
+			return CVEC_FORCECAP_FAIL;
+
+		if (!_vec->forceCap)
+			_vec->cap += __CVEC_CAP_ADDITION;
+
+		void *newData = realloc(_vec->data, _vec->cap * _vec->elemLen);
+
+		if (!newData)
+			return CVEC_FAIL_ALLOCATION;
+
+		_vec->data = newData;
+	}
+
+	memcpy((char*)_vec->data + _vec->size * _vec->elemLen, _elem, _vec->elemLen);
+
+	_vec->size++;
+
+	return CVEC_SUCCESS;
+}
+
+int __cvec_pushFront(CVEC *_vec, void *_elem)
+{
+	if (!_vec || !_elem) return CVEC_FAIL;
+
+	// Expand capacity if needed
+	if (_vec->size >= _vec->cap)
+	{
+		if (_vec->forceCap)
+			return CVEC_FORCECAP_FAIL;
+
+		if (!_vec->forceCap && _vec->dynamicCap) _vec->cap += __CVEC_CAP_ADDITION;
+
+		void *newData = realloc(_vec->data, _vec->cap * _vec->elemLen);
+
+		if (!newData)
+			return CVEC_FAIL_ALLOCATION;
+
+		_vec->data = newData;
+	}
+
+	// Shift elements one slot to the right
+	if (_vec->size > 0)
+	{
+		memmove(
+			(char*)_vec->data + _vec->elemLen,    // destination start (index 1)
+			_vec->data,                           // source start (index 0)
+			_vec->size * _vec->elemLen            // number of bytes to move
+		);
+	}
+
+	// Copy new element to index 0
+	memcpy(_vec->data, _elem, _vec->elemLen);
+
+	_vec->size++;
+
+	return CVEC_SUCCESS;
+}
+
+int __cvec_pushIndex(CVEC *_vec, const size_t _index, void *_elem)
+{
+	if (!_vec || !_elem) return CVEC_FAIL;
+
+	// Bounds: inserting at size is allowed (append),
+	// anything larger is out of range.
+	if (_index > _vec->size)
+		return CVEC_FAIL; // or define a specific error code
+
+	// Expand capacity if needed
+	if (_vec->size >= _vec->cap)
+	{
+		if (_vec->forceCap)
+			return CVEC_FORCECAP_FAIL;
+
+		if (!_vec->forceCap && _vec->dynamicCap) _vec->cap += __CVEC_CAP_ADDITION;
+
+		void *newData = realloc(_vec->data, _vec->cap * _vec->elemLen);
+
+		if (!newData)
+			return CVEC_FAIL_ALLOCATION;
+
+		_vec->data = newData;
+	}
+
+	// If not inserting at the end, shift elements to the right
+	if (_index < _vec->size)
+	{
+		memmove(
+			(char*)_vec->data + (_index + __CVEC_CAP_ADDITION) * _vec->elemLen,
+			(char*)_vec->data + _index * _vec->elemLen,
+			(_vec->size - _index) * _vec->elemLen
+		);
+	}
+
+	// Copy new element into the index
+	memcpy(
+		(char*)_vec->data + _index * _vec->elemLen,
+		_elem,
+		_vec->elemLen
+	);
+
+	_vec->size++;
+	return CVEC_SUCCESS;
+}
+
+bool cvec_at(const CVEC *_vec, const size_t _index)
+{
+	if (!_vec) return false;
+
+	if (_vec->size >= _index)
+		return true;
+
+	return false;
+}
+
+bool cvec_atCap(const CVEC *_vec, const size_t _index)
+{
+	if (!_vec) return false;
+
+	if (_vec->cap >= _index)
+		return true;
+
+	return false;
+}
+
+void *cvec_get(const CVEC *_vec, const size_t _index)
+{
+	// Error: Out of bounds
+	if (_index >= _vec->size ||
+			!_vec || !_vec->data)
+		return NULL;
+
+	return (char*)_vec->data + _index * _vec->elemLen;
+}
+
+int cvec_merge(CVEC *_toMerge, const CVEC *_input)
+{
+	if (!_toMerge || !_input || !_input->data)
+		return CVEC_FAIL;
+
+	for (unsigned int i = 0; i < _input->size; i++)
+	{
+		void *elem = cvec_get(_input, i);
+
+		if (!elem)
+			continue;
+
+		__cvec_push(_toMerge, elem);
+	}
+
+	return CVEC_SUCCESS;
+}
+
+int __cvec_set(CVEC *_vec, const size_t _index, void *_set)
+{
+	if (!_vec || !_vec->data) return CVEC_FAIL;
+
+	if (_index >= _vec->cap) return CVEC_OUT_BOUNDS;
+
+	CVEC old = *_vec;
+	CVEC replacement = cvec_init(old.cap, old.elemLen);
+
+	bool isSet = false;
+
+	for (unsigned int i = 0 ; i < old.size ; i++)
+	{
+		void *foundElem = cvec_get(&old, i);
+
+		if (!foundElem) continue;
+
+		if (!isSet && i == _index)
+		{
+			__cvec_push(&replacement, _set);
+
+			isSet = true;
+
+			continue;
+		}
+
+		// Leave it as is
+		__cvec_push(&replacement, foundElem);
+	}
+
+	// Fill the rest
+
+	*_vec = replacement;
+
+	cvec_destroy(&old);
+
+	return CVEC_SUCCESS;
+}
+
+CVEC cvec_initCopy(const CVEC *_src)
+{
+	CVEC dest;
+
+	// Set error, return an empty copy without initializement
+	if (!_src || !_src->data || !_src->initialized)
+	{
+		__CVEC_SET_NULL(dest, .);
+
+		return dest;
+	}
+
+	dest = cvec_init(_src->cap, _src->elemLen);
+
+	if (!dest.initialized)
+		return dest; // Return the `dest` as is
+
+	memcpy(dest.data, _src->data, _src->size * _src->elemLen);
+
+	dest.size = _src->size;
+	dest.forceCap = _src->forceCap;
+
+	return dest;
+}
+
+// del() functions
+int __cvec_delIndex(CVEC *_vec, void *_elem, const size_t start)
+{
+    if (!_vec || !_elem) return CVEC_FAIL;
+
+    CVEC old = *_vec;  // Shallow Copy
+    CVEC retVec = cvec_init(old.cap, old.elemLen);
+
+    bool removed = false;
+
+    for (unsigned int i = 0; i < old.size; i++)
+    {
+        void *val = cvec_get(&old, i);
+
+        // Once reached the start index and have not removed anything yet, do the following if condition
+        if (!removed && i >= start)
+        {
+            if (memcmp(val, _elem, old.elemLen) == 0)
+            { removed = true; continue; }
+        }
+
+        // Copy all others as is
+        __cvec_push(&retVec, val);
+    }
+
+    if (!removed)
+    {
+        cvec_destroy(&retVec);
+
+        return CVEC_ELEM_NOT_FOUND;
+    }
+
+    *_vec = retVec;
+
+    // Free `old` vector data
+    cvec_destroy(&old);
+
+    return CVEC_SUCCESS;
+}
+
+int __cvec_del(CVEC *_vec, void *_elem)
+{
+	if (!_vec || !_elem) return CVEC_FAIL;
+
+	// Start from index 0
+	// Delete element
+	// Repeat until reached the size limit
+	for (unsigned int i = 0 ; i < _vec->size ; i++)
+	{ __cvec_delIndex(_vec, _elem, 0); }
+
+	return CVEC_SUCCESS;
+}
+
+int __cvec_find(const CVEC *_vec, void *_find)
+{
+	if (!_vec || !_find)
+		return CVEC_FAIL;
+
+	for (unsigned int i = 0 ; i < _vec->size ; i++)
+	{
+        void *val = cvec_get(_vec, i);
+
+        if (memcmp(val, _find, _vec->elemLen) == 0)
+        	return i;
+	}
+
+	return CVEC_NPOS;
+}
+
+int cvec_remove(CVEC *_vec, const size_t _index)
+{
+	if (!_vec) return CVEC_FAIL;
+
+	CVEC old = *_vec;
+	CVEC retVec = cvec_init(old.cap, old.elemLen);
+
+	for (unsigned int i = 0 ; i < old.size ; i++)
+	{
+		void *foundElem = cvec_get(&old, i);
+
+		// Element to delete will be ignored
+		if (_index == i)
+			continue;
+
+		__cvec_push(&retVec, foundElem);
+	}
+
+	*_vec = retVec;
+
+	cvec_destroy(&old);
+
+	return CVEC_SUCCESS;
+}
+
+int cvec_popBack(CVEC *_vec)
+{
+	if (!_vec)
+		return CVEC_FAIL;
+
+	cvec_remove(_vec, _vec->size - 1);
+
+	return CVEC_SUCCESS;
+}
+
+int cvec_popFront(CVEC *_vec)
+{
+	if (!_vec)
+		return CVEC_FAIL;
+
+	cvec_remove(_vec, 0);
+
+	return CVEC_SUCCESS;
+}
+
+int cvec_reverse(CVEC *_vec)
+{
+    if (!_vec || !_vec->data)
+        return CVEC_FAIL;
+
+    CVEC old = *_vec;  // Shallow Copy
+    CVEC tmpVec = cvec_init(old.cap, old.elemLen);
+
+    // Reverse the elements by iterating backward through `_vec`
+    for (int i = old.size - 1; i >= 0; i--)
+    {
+        void *result = cvec_get(&old, i);
+
+        if (!result)
+        {
+        	cvec_destroy(&tmpVec);
+
+            return CVEC_GET_NULL;
+        }
+
+        __cvec_push(&tmpVec, result);
+    }
+
+    *_vec = tmpVec;
+
+    cvec_destroy(&old);
+
+    return CVEC_SUCCESS;
+}
+
+int cvec_shrink(CVEC *_vec)
+{
+	if (!_vec || !_vec->data) return CVEC_FAIL;
+
+	if (_vec->cap < _vec->size)
+		_vec->cap = _vec->size + __CVEC_CAP_ADDITION;
+
+	return CVEC_SUCCESS;
+}
+
+bool __cvec_hasEnoughCap(const CVEC *_vec, const size_t _additions)
+{
+    if (!_vec) return false;
+
+    if (_vec->dynamicCap)
+    	return true;
+
+    return _vec->size  <= _vec->cap - _additions;
+}
+
